be920c8e34e0672812680370194d97136dc9ac45
Mateo-Sanchez14 <mateo-sanchez@hotmail.com>
2024-08-16 00:38:35 -0300

    add branding

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { Typography } from '@mui/material';
 import './App.css';
 import OrderEmpanadas from './OrderEmpanadas';
 
 function App() {
   return (
     <div className="App">
+      <Typography variant="h1" component="h2">
+ Empanadas para todos
+</Typography>
       <OrderEmpanadas />
     </div>
   );
